bd3495d53fe6ebe9c5e03425051d93450394e419
palfre2rm <47236146+palfre2rm@users.noreply.github.com>
2019-02-07 20:12:55 -0800

    Assignament 01
    2/07/2019

---
+++ js/script.js
@@ -0,0 +1,199 @@
+/*ESLint-env BROWSER*/
+
+//STEP 1
+
+var someMonth;
+//cfunction theMonth();
+var currentMonth;
+var summerMonth;
+//var MyLibraryFunction();
+
+
+//STEP 2
+
+x = 456;
+x = "JOE";
+x = false;
+x = null;
+
+//STEP 3
+var anExpression = 3 * (4 / 5) + 6;
+var anotherExpression = Math.PI;
+
+
+//STEP  4
+
+var firstName;  
+var lastName; 
+var address;
+var city; 
+var state; 
+var zipCode;
+var yourAge; 
+var ferralSource; 
+var MayWeContactYou;
+
+
+//STEP 5
+
+var address;
+address = "55 San Diego Rd";
+
+var address = "55 San Diego Rd";
+
+var address = "55 San Diego Rd", city = "San Diego";
+
+
+//STEP 6
+
+var x;
+x = 2;
+window.console.log("2 is this number is: " + x);
+
+
+//STEP 7
+
+var x = true;
+window.console.log("The result is : " + x);
+
+var y = 2;
+window.console.log("The result is : " + y);
+var y = Boolean;
+window.console.log("The result is : " + y);
+
+//STEP 8
+
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I am not sure about the former."';
+window.console.log(someString);
+
+
+//STEP 9
+
+var x = 200;
+var y = null;
+x = null;
+window.console.log(x);
+
+
+var x;
+window.console.log(x);
+
+
+//STEP 10
+
+typeof "John" 
+typeof 3.14
+typeof false 
+typeof [1,2,3,4]
+typeof myCar 
+
+
+//STEP 11
+
+window.alert("Hello Zak Ruvalcaba," + "welcome to the JavaScript class!");
+
+window.alert("Hello " + "Alfredo Arellano" + ", welcome to the JavaScript class!");
+
+
+//STEP 12
+
+var name = "Alfredo Arellano";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");
+
+
+//STEP 13
+
+var course ="JavaScript";
+window.alert("Hello " + "Alfredo ArelLano" + "Welcome to the " + course + " Class");
+
+
+//STEP 14
+
+window.alert("Hello " + "Alfredo ArelLano\n" + "Welcome to the " + course + " Class");
+
+
+//STEP 15
+
+
+var name = window.prompt("what is your name? ");
+window.alert("Hello " + name + "\n" + "Welcome to the " + course + " Class");
+
+
+//STEP 16
+var course = window.prompt("what is your course? ");
+window.alert("Hello " + name + "\n" + "Welcome to the " + course + " Class");
+
+
+//STEP 17
+
+var x;
+x = 10;
+
+var y;
+y = 20;
+
+window.console.log(x + y);
+
+
+//STEP 18
+
+var x = 20;
+window.console.log(x = x + 20);
+
+
+//STEP 19
+
+
+var x = 20;
+window.console.log(x *= 5);
+
+
+//STEP 20
+
+var x = (20 % 3)
+x /= 1;
+window.console.log(x);
+
+
+//STEP 21 
+
+var x = 200;
+var y = 200;
+window.console.log(x == y);
+
+
+//STEP 22
+
+var x = 200;
+var y = 200;
+window.console.log(x < y);
+
+
+//STEP 23
+
+var widget = new Object();
+window.console.log(typeof Object);
+
+//STEP 24
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+}
+var auto = new Car('Honda', 'Accord', 1998);
+
+
+console.log(auto instanceof Object);
+
+
+//STEP 25
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+}
+var auto = new Car('Honda', 'Accord', 1998);
+
+console.log(honda instanceof Object);
